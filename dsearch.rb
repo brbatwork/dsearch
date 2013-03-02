2a059746370ad506d844d7ae473c2913eae4db1c
Bill <brbatwork@gmail.com>
2013-03-02 16:19:31 -0500

    Implemented a queue - now only need to properly throttle it

--- dsearch.rb
+++ dsearch.rb
@@ -2,6 +2,7 @@
 require 'whois'
 require 'open-uri'
 require 'optparse'
+require 'thread'
 
 options = {}
 option_parser = OptionParser.new do |opts|
@@ -22,14 +23,15 @@ file = open("http://www.scrabblefinder.com/ends-with/#{tld}/")
 content = file.read
 words = content.scan(/\/word\/[a-z]+#{tld}/)
 
+queue = Queue.new
 puts "Searching #{words.size} possible domains"
 words.each { |x|
   begin
     word = x.slice(/[a-z]+#{tld}$/)
     domain = word.sub(/#{tld}$/, ".#{tld}")
-    r = Whois.whois(domain)
-    puts "#{word} --> #{domain}" if r.available?
-    sleep(1/2)
+	wd = [word, domain]
+	queue << wd
+	sleep (1/2)
   rescue SignalException => e
     STDOUT.flush
     STDERR.puts "Quitting"
@@ -41,3 +43,17 @@ words.each { |x|
     STDERR.flush
   end
 }
+
+consumer = Thread.new("Whois consumer") do |name|
+	until queue.empty?
+		wait = [((queue.size % 10) - 10).abs, 5].min #TODO need better throttle algorthim
+		#puts "Sleeping #{wait} secs"
+		sleep(wait)
+		wd = queue.pop
+		r = Whois.whois(wd[1])
+		puts "#{wd[0]} --> #{wd[1]}" if r.available?		
+	end
+
+end
+
+consumer.join
